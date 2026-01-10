761be7e2ba27f57f2d0ec1d000586a0b78820ea3
Edmilson Constantino <edmilsonconstantino879@gmail.com>
2026-01-10 12:37:07 +0200

    cs

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 # exit on error
-#!/usr/bin/env bash
 set -o errexit
 
 pip install -r requirements.txt
-mkdir -p /opt/render/project/src
-python manage.py migrate
+
 python manage.py collectstatic --no-input
+python manage.py migrate
